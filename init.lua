3fb318adaf8c0bc3b25025b2ac1fa167c81d2dd9
Linus  Silberstein <l.silberstein@ostfalia.de>
2023-03-13 19:33:40 +0100

    outsourced options

--- init.lua
+++ init.lua
@@ -1,14 +1,4 @@
--- options
-vim.opt.number = true
-vim.opt.mouse = 'a'
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.hlsearch = true
-vim.opt.wrap = true
-vim.opt.breakindent = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = false
+require('user.options')
 
 -- keybinds
 vim.g.mapleader = ' '
---
+++ user/options.lua
@@ -0,0 +1,11 @@
+-- options
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.wrap = true
+vim.opt.breakindent = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = false
